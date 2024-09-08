87e4252ef7710f206ef8eff52e8192d7cee735c9
raghavmanish <mnsyd24@gmail.com>
2024-09-08 17:48:31 +0530

    added city middleware

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,9 +1,10 @@
 const express = require('express') ;
 const {CityController} = require('../../controllers') ;
 const router = express.Router() ;
+const {CityMiddleware} = require('../../middlewares') ;
 
 // /api/v1/cities POST request
-router.post('/' , CityController.createCity) ; // yes here you have to call the createAirplane function of the airplane-controller.js file in controller folder
+router.post('/' ,CityMiddleware.validateRequest , CityController.createCity) ; // yes here you have to call the createAirplane function of the airplane-controller.js file in controller folder
 
 
 module.exports = router ;
